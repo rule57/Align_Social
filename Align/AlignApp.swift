3533e75b44dec083ad38d7f2b7518e336d904582
rule57 <wjr5232@psu.edu>
2024-06-21 21:59:16 -0400

    Firebase but supabase is still here shittin around

--- Align/AlignApp.swift
+++ Align/AlignApp.swift
@@ -4,14 +4,22 @@
 //
 //  Created by William Rule on 6/16/24.
 //
+// Gemini API Code - AIzaSyBmW81r2vCCEttGLaYIedfYb4Oph93Zu-A
 
 import SwiftUI
+import FirebaseCore
 import Supabase
 
-@main
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
 
-
+    return true
+  }
+}
 struct AlignApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             SignInView()
